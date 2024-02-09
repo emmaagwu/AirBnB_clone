5bf50595bcb1d069387582f0e7223c1d471df828
ogeeke <ogechieke2014@gmail.com>
2024-02-09 14:29:29 -0800

    state

---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Defines the "state" class """
+from model.base_model import Basemodel
+
+
+class State(Basemodel):
+    """ Represents a State
+    Attributes:
+        name(str) - name of the state
+    """
+
+    state = ""
