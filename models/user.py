b4a3868bc1fbf9762d49337ae11e82e9fa3f209b
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2024-02-09 14:13:53 -0800

    user

--- models/user.py
+++ models/user.py
@@ -1,1 +1,18 @@
-#!/usr/bin/env
+#!/usr/bin/env python3
+""" Defines the "USER" class """
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """ Represents a User
+    Attributes:
+        email(str) - email of the user
+        password(str) - password of the user
+        first_name(str) - first name of the user
+        last_name(str) - Last name of the user
+    """
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
