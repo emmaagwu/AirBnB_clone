579d5c063e247f5015b1d9d259d29d21fd4b5002
ogeeke <ogechieke2014@gmail.com>
2024-02-09 14:55:57 -0800

    amenity.py

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/env/ pythom3
+""" Defines the "Amenity" class """
+from models.base_model import Basemodel
+
+
+class Amenity(Basemodel):
+    """Represents an Amenity
+    Attribute:
+        name(str) - name of an Amenity
+     """
+
+    name = ""
