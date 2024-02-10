f15b6f1d0cf096911219d9c808ec88e552f1a177
ogeeke <ogechieke2014@gmail.com>
2024-02-10 14:19:54 -0800

    base_model.py

---
+++ models/base_model.py
@@ -0,0 +1,46 @@
+#!usr/bin/env python3
+""" Defines all common attributes/methods for other classes """
+import models
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """ Defines all common attributes/methods for other classes """
+    def __init__(self, *args, **kwargs):
+        """ initiates the BaseModel class
+        Arguments:
+            args(any) - unused
+            kargs(dict) - Dictionary representation of a class
+        """
+        timeform = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        self.__dict__[key] = datetime.strptime(value, timeform)
+                    else:
+                        self.__dict__[key] = value
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(sel)
+
+    def save(self):
+        """ update BaseModel"""
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """ returns a dictionary representaion of BaseMOdel """
+        dict_copy = self.__dict__.copy()
+        dict_copy["created_at"] = self.created_at.isoformat()
+        dict_copy["updated_at"] = self.updated_at.isoformat()
+        dict_copy["__class__"] = self.__class__.__name__
+        return dict_copy
+
+    def __str__(self):
+        """ Returns the string representation of BaseModel """
+        classname = self.__class__.__name__
+        return "[{}] ({}) {}".format(classname, self.id, self.__dict__)
