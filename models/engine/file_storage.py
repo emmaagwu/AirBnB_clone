0a3efa348cfd1133756576a04e2fa7771fa35aa8
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2024-02-11 13:00:18 -0800

    AirBnB

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,19 +11,18 @@ from models.review import Review
 
 
 class FileStorage:
-    """ Serializes instances to a JSON file and deserializes JSON file to instances
-   
+    """ Serializes and Deserializes instances to a JSON file.
     Attributes:
         __file_path (str): The name of the file to save objects to.
         __objects (dict): A dictionary of instantiated objects.
     """
     __file_path = "file.json"
     __objects = {}
-    
+
     def all(self):
         """ Returns the dictionary """
         return FileStorage.__objects
-   
+
     def new(self, obj):
         """ sets in __objects the obj with key <obj class name>.id """
         objkey = "{}.{}".format(obj.__class__.__name__, obj.id)
@@ -31,19 +30,19 @@ class FileStorage:
 
     def save(self):
         """ serializes __objects to the JSON file """
-        compactdict = FileStorage.__objects
-        expandeddict = {key: value.to_dict() for key, value in compactdict.items()}
+        cptdict = FileStorage.__objects
+        expandeddict = {key: value.to_dict() for key, value in cptdict.items()}
         with open(FileStorage.__file_path, "w") as jsonfile:
             json.dump(expandeddict, jsonfile)
 
     def reload(self):
-        """ deserializes the JSON file to __objects (only if the JSON file (__file_path) """
+        """ deserializes the JSON file."""
         try:
             with open(FileStorage.__file_path, "r") as jsonfile:
                 expandeddict = json.load(jsonfile)
             for key, value in expandeddict.items():
-               classname = key.split('.')[0]
-               del value["__class__"]
-               self.new(eval(classname)(**value))
+                classname = key.split('.')[0]
+                del value["__class__"]
+                self.new(eval(classname)(**value))
         except FileNotFoundError:
             return
